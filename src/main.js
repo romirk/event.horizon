462cee21c2641cb5f2889afc01b93cbf157a911f
romir.k <romir.kulshrestha@gmail.com>
2018-07-26 20:17:06 +0530

    Update Main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var http = require('http');
+
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.collection("users").findOne({}, function(err, result) {
+  var testdb = db.db("testdb");
+  testdb.collection("users").findOne({}, function(err, result) {
     if (err) throw err;
+    http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Hello World!\n<br>'+result.name);
+    }).listen(8080);
     console.log(result.name);
     db.close();
   });
