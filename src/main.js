64c1482301c8fa47a5c3e7c192c4b48ac7e198b0
Imagifight <romir.kulshrestha@gmail.com>
2018-07-26 19:19:33 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("mydb");
-  dbo.collection("customers").findOne({}, function(err, result) {
+  dbo.collection("users").findOne({}, function(err, result) {
     if (err) throw err;
     console.log(result.name);
     db.close();
