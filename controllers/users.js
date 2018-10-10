90b7fbed2e00bb701bc0e5a56c6af36a4e6de79c
romir.k <romir.kulshrestha@gmail.com>
2018-10-10 20:09:57 +0530

    IMPORTANT: Reorganized User Authentication
    Shifted 'user/auth' post (i.e. user authentication) from user controller to user model as an API function, so as to let authenticate.js access it.
    TODO:
    1) change authenticate.js to use this format
    2) debug error handling
    Additional: email format test using RegEx

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const saltRounds = 16;
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
@@ -40,65 +40,54 @@ router.get('/:un?', (req, res) => {
 });
 router.post('/auth', (req, res) => {
     username = req.body.username;
-    password = req.body.username;
-    user.get({ username: username }, (err, u) => {
-        if (err) {
-            res.json({ success: false, message: `Failed to load users. Error: ${err}` });
-        }
-        else if (!u) {
-            res.json({ success: false, message: `Failed to load users. Error: No user with username ${username}` });
+    password = req.body.password;
+    var success, message;
+    user.auth(username, password, async function (s, m) {
+        success = s;
+        message = m;
+        console.log('then', success, message);
+        await res.json({ success: success, message: message });
+    });
+});
+router.post('/new', (req, res) => {
+    bcrypt.hash(req.body.pass, 16, function (err, hash) {
+        if (err) { throw (err); }
+        if (req.body.email.test(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+            bcrypt.compare(req.body.pass, hash, function (err, result) {
+                if (err) { throw (err); }
+                let newUser = new user({
+                    name: req.body.name,
+                    email: req.body.email,
+                    pass: hash,
+                    grade: req.body.grade,
+                    sec: req.body.sec,
+                    status: req.body.status,
+                    username: req.body.username
+                });
+                console.log(`REG\t${newUser}`);
+                user.register(newUser, (err, u) => {
+                    if (err) {
+                        res.json({ success: false, message: `Failed to create a new user. Error: ${err}\n ${newUser}` });
+                    }
+                    else
+                        res.json({ success: true, message: "Added successfully." });
+
+                });
+            });
         }
         else {
-            bcrypt.compare(password, u.pass, function (err, r) {
-                console.log(u.pass, password);
-                if (err) {
-                    res.json({ success: false, message: `Failed to authenticate user. Error: ${err}\n` });
-                }
-                else if (!r) {
-                    res.json({ success: false, message: `Failed to authenticate user. Error: ${err}\n${u}` });
-                }
-                else {
-                    res.json({ success: true, message: "Authenticated successfully." });
-                }
-            });
+            res.json({ success: false, message: `Invalid email address.` });
         }
-    })
-})
-router.post('/new', (req, res) => {
-    bcrypt.hash(req.body.pass, 10, function (err, hash) {
-        if (err) { throw (err); }
-
-        bcrypt.compare(req.body.pass, hash, function (err, result) {
-            if (err) { throw (err); }
-            let newUser = new user({
-                name: req.body.name,
-                email: req.body.email,
-                pass: hash,
-                grade: req.body.grade,
-                sec: req.body.sec,
-                status: req.body.status,
-                username: req.body.username
-            });
-            console.log(`REG\t${newUser}`);
-            user.register(newUser, (err, u) => {
-                if (err) {
-                    res.json({ success: false, message: `Failed to create a new user. Error: ${err}\n ${newUser}` });
-                }
-                else
-                    res.json({ success: true, message: "Added successfully." });
-
-            });
-        });
     });
 });
 router.post('/edit/:id', (req, res) => {
     let id = req.params.id;
     let update = req.body.update;
-    (async function(){
-        if(req.body.update.pass) {
-            const h = await bcrypt.hash(req.body.update.pass, 10)//, function (err, hash) {
+    (async function () {
+        if (req.body.update.pass) {
+            const h = await bcrypt.hash(req.body.update.pass, 16)//, function (err, hash) {
             //     if (err) { throw (err); }
-        
+
             //     bcrypt.compare(req.body.update.pass, hash, function (err, result) {
             //         if (err) { throw (err); }
             //         if(result) update.pass = hash;
@@ -108,7 +97,7 @@ router.post('/edit/:id', (req, res) => {
             // });
             update.pass = h;
         }
-    
+
         console.log(`UPDATE ${id}\t${JSON.stringify(update)}`);
         await user.edit({ _id: id }, update, (err, updatedUser) => {
             if (err) {
@@ -122,7 +111,7 @@ router.post('/edit/:id', (req, res) => {
                 res.end();
             }
         })
-    })().catch(err => {throw err;});
+    })().catch(err => { throw err; });
 })
 router.delete('/:id', (req, res) => {
     //access the parameter which is the id of the item to be deleted
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 //Require mongoose package
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
 
 //Define UserSchema with title, description and category
 const UserSchema = mongoose.Schema({
@@ -53,4 +55,38 @@ module.exports.edit = (conditions, update, callback) => {
 module.exports.delete = (id, callback) => {
     let query = { _id: id };
     User.deleteOne(query, callback);
+}
+module.exports.auth = (username, password, callback) => {
+    err = false;
+    module.exports.get({ username: username }, (err, u) => {
+        var success, message;
+        (async function () {
+            if (err) {
+                success = false;
+                message = `Failed to load users. Error: ${err}`;
+            }
+            else if (!u) {
+                success = false;
+                message = `Failed to load users. Error: No user with username ${username}`;
+            }
+            else {
+                await bcrypt.compare(password, u.pass).then((r) => {
+                    console.log("u.pass", u.pass, "password", password, r);
+                    if (err) {
+                        success = false;
+                        message = `Failed to authenticate user. Error: ${err}`;
+                    }
+                    else if (!r) {
+                        success = false;
+                        message = `Failed to authenticate user. Error: ${err}`;
+                    }
+                    else {
+                        success = true;
+                        message = "Authenticated successfully.";
+                    }
+                });
+            }
+            await callback(success, message);
+        })();
+    });
 }
