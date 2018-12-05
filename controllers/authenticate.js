1efb5e69341d5220b5485ec01ab86b6f9d2ab326
romir.k <romir.kulshrestha@gmail.com>
2018-12-05 19:39:35 +0530

    fixed login, jwt

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -3,12 +3,36 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 const user = require('../models/user');
 const config = require('../config/config');
+
+router.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ 
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+ 
+    // Pass to next layer of middleware
+    next();
+ });
+ 
+ router.use(cors({origin:'*'}));
+
 
 router.post('/', (req, res) => {
     user.get({ username: req.body.username }, (err, u) => {
--- src/app/dash.js
+++ src/app/dash.js
@@ -1,7 +1,7 @@
 const url = "http://lvh.me:3000/event";
 var jsonData, events;
 
-var app = angular.module('event.horizon', ['ngMaterial', 'ngMessages']);
+var app = angular.module('event.horizon', ['ngMaterial', 'ngMessages', 'ngCookies']);
 
 app.config(['$mdIconProvider', function ($mdIconProvider) {
         $mdIconProvider.icon('md-toggle-arrow', 'img/icons/toggle-arrow.svg', 48);
@@ -12,7 +12,7 @@ app.config(['$mdIconProvider', function ($mdIconProvider) {
             .accentPalette('pink');
         $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
     })
-    .controller('events', function ($scope, $http) {
+    .controller('events', function ($scope, $http, $cookies) {
         $scope.points = {
             c: 187,
             e: 190,
@@ -31,6 +31,7 @@ app.config(['$mdIconProvider', function ($mdIconProvider) {
             },
             (err) => {
                 console.log(err);
+                document.getElementById('cardwrap').innerHTML = "<h2 style='margin:auto; text-align:center'><code style='color: #444'>no events</code></h2>";
             }
         )
 
@@ -39,7 +40,31 @@ app.config(['$mdIconProvider', function ($mdIconProvider) {
             document.getElementById('id01').style.display = 'block';
         }
 
-
+        $scope.init = () => {
+            console.log(localStorage.getItem('jwt'));
+            if (localStorage.getItem('jwt')/*$cookies.get('jwt')*/)
+                $http.post("http://lvh.me:3000/auth/verify", {
+                    token: localStorage.getItem('jwt')
+                }).then((res) => {
+                    if (!res.data.success) {
+                        localStorage.removeItem('jwt');
+                        window.location.replace("login.html");
+                    }
+                    else {
+                        $scope.payload = res.data.payload;
+                    }
+                }, () => {
+                    window.location.replace("login.html");
+                });
+            else{
+                window.location.replace("login.html");
+            }
+                
+        }
+        $scope.logOut = () => {
+            localStorage.removeItem('jwt');
+            window.location.replace("login.html");
+        }
     });
 
 function formatDate(date) {
@@ -56,7 +81,4 @@ function formatDate(date) {
     var year = date.getFullYear();
 
     return monthNames[monthIndex] + ' ' + day + ', ' + year;
-}
-window.onscroll = () => {
-
 }
